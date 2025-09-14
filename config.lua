46347ac15a67f953a91965fd7c9da3c8c697285f
jgyugu <93074931+jgyugu@users.noreply.github.com>
2025-09-14 19:02:59 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,5 +5,8 @@ return {
 这是一个收集可用脚本的脚本中心，方便快速运行。
 此脚本完全免费，请勿倒卖！
     ]],
-    key = "666-ethan-yyds"
+    key = "666-ethan-yyds", -- 卡密
+    blacklist = {           -- 黑名单 UID 列表
+        
+    }
 }
