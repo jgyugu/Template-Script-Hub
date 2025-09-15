de54761814889920dd353c3bec36a3f75cacf120
jgyugu <93074931+jgyugu@users.noreply.github.com>
2025-09-15 18:58:54 +0800

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -1,10 +1,18 @@
-return {
+local scripts = {
     {
         name = "飞行脚本",
-        url = "https://raw.githubusercontent.com/jgyugu/Template-Script-Hub/refs/heads/main/fly_Script"
+        url = "https://raw.githubusercontent.com/jgyugu/Template-Script-Hub/refs/heads/main/fly_Script",
+        type = "通用"
     },
     {
         name = "飞车脚本",
-        url = "https://raw.githubusercontent.com/jgyugu/Template-Script-Hub/refs/heads/main/Vehicle_Fly_Script"
-    }
+        url = "https://raw.githubusercontent.com/jgyugu/Template-Script-Hub/refs/heads/main/Vehicle_Fly_Script",
+        type = "通用"
+    },
+    {
+        name = "力量传奇(暂时没用)",
+        url = "https://占位符",
+        type = "力量传奇",
+        id = 114514267
+    },
 }
