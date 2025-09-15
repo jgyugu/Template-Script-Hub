db08f9936169b038ca2caae632576b4fc2f08e00
jgyugu <93074931+jgyugu@users.noreply.github.com>
2025-09-15 13:56:52 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,13 +1,11 @@
 return {
-    version = "v1.2", -- 版本号
+    version = "v1.3", -- 版本号
     announcement = [[
 欢迎来到 ethan脚本中心！
-这是一个收集可用脚本的脚本中心，方便快速运行。
+这是一个收集可用脚本的脚本中心方便快速运行。
 此脚本完全免费请勿倒卖！
-    ]],
+我有时间就会更新的!]], -- 公告
     key = "666-ethan-yyds", -- 卡密
-    blacklist = {           -- 黑名单 UID 列表
-        
-    },
+    blacklist = {}, -- 黑名单
     scriptHubURL = "https://raw.githubusercontent.com/jgyugu/Template-Script-Hub/refs/heads/main/menu.lua" -- 脚本中心主入口
 }
