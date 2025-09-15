9e531eab35ff8cdb2e2942aa51cdbda807bed3e3
jgyugu <93074931+jgyugu@users.noreply.github.com>
2025-09-16 07:24:28 +0800

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -8,11 +8,5 @@ return {
         name = "飞车脚本",
         url = "https://raw.githubusercontent.com/jgyugu/Template-Script-Hub/refs/heads/main/Vehicle_Fly_Script",
         type = "通用"
-    },
-    {
-        name = "让所有玩家爆炸",
-        url = "https://raw.githubusercontent.com/jgyugu/Template-Script-Hub/refs/heads/main/boom",
-        type = "娱乐(仅供我的作品)",
-        id = 6689388756
     }
 }
